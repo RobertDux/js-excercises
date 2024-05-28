d4f2e25b185a12ac023afdc817b2ab7b91f3e8f3
RobertDux <126151329+RobertDux@users.noreply.github.com>
2024-05-28 10:43:57 +0200

    refactor(dom): create helper function to build elements

--- apps/weather-app/src/scripts/dom.js
+++ apps/weather-app/src/scripts/dom.js
@@ -1,38 +1,75 @@
 const locationsElem = document.querySelector(".js-locations");
 
+/**
+ * Creates an HTML element with provided classes and/or text
+ * @param {String} tag
+ * @param {Array} classes
+ * @param {String} text
+ * @returns Element
+ */
+function buildElement(tag, classes = [], text = null) {
+  const elem = document.createElement(tag);
+
+  if (classes.length > 0) {
+    elem.classList.add(...classes);
+  }
+
+  if (text) {
+    elem.textContent = text;
+  }
+
+  return elem;
+}
+
+/**
+ * Builds the DOM by adding cards with location and weather data
+ * @param {Object} data
+ * @param {Function} removeFn
+ */
 function renderLocation({ location, weather }, removeFn) {
   const name = location.name.length > 0 ? location.name : location.city;
 
-  const container = document.createElement("div");
-  container.classList.add("col-sm-4");
-
-  const card = document.createElement("div");
-  card.classList.add("card");
+  const container = buildElement("div", ["col-sm-4"]);
+  const card = buildElement("div", ["card"]);
   container.appendChild(card);
 
-  const cardHeader = document.createElement("div");
-  cardHeader.classList.add("card-header");
-  cardHeader.textContent = name;
-  card.appendChild(cardHeader);
-
-  const cardBody = document.createElement("div");
-  cardBody.classList.add("card-body");
-  card.appendChild(cardBody);
-
-  const cardTitle = document.createElement("h3");
-  cardTitle.classList.add("lead");
-  cardTitle.textContent = location.city;
-  cardBody.appendChild(cardTitle);
-
-  const cardText = document.createElement("p");
-  cardText.classList.add("my-4");
-  cardText.textContent = "Temperature: " + weather.temperature + " degrees.";
-  cardBody.appendChild(cardText);
-
-  const cardButton = document.createElement("button");
-  cardButton.classList.add("btn", "btn-sm", "btn-danger");
-  cardButton.textContent = "Remove";
+  // Card
+  const cardHeader = buildElement("div", ["card-header"], name);
+  const cardBody = buildElement("div", ["card-body"]);
+  const cardTitle = buildElement("h3", ["lead"], location.city);
+  const image = buildElement("img");
+  image.setAttribute(
+    "src",
+    "https://openweathermap.org/img/wn/" + weather.icon + "@2x.png"
+  );
+  const general = buildElement(
+    "p",
+    ["m-0"],
+    "General: " + weather.general + "."
+  );
+  const temperature = buildElement(
+    "p",
+    ["m-0"],
+    "Temperature: " + weather.temperature + " degrees."
+  );
+  const humidity = buildElement(
+    "p",
+    "",
+    "Humidity: " + weather.humidity + "%."
+  );
+  const cardButton = buildElement(
+    "button",
+    ["btn", "btn-sm", "btn-danger"],
+    "Remove"
+  );
   cardButton.addEventListener("click", () => removeFn(location.key), false);
+  card.appendChild(cardHeader);
+  card.appendChild(cardBody);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(image);
+  cardBody.appendChild(general);
+  cardBody.appendChild(temperature);
+  cardBody.appendChild(humidity);
   cardBody.appendChild(cardButton);
 
   locationsElem.appendChild(container);
