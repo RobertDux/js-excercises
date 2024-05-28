2ff9e7f3d44f3f27e460a5118c6191ab59ba8bc7
RobertDux <126151329+RobertDux@users.noreply.github.com>
2024-05-28 15:51:50 +0200

    refactor(api): create utility function to KISS

--- apps/weather-app/src/scripts/weather.js
+++ apps/weather-app/src/scripts/weather.js
@@ -2,49 +2,14 @@ const API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = process.env.API_KEY;
 
 /**
- * Creates a human-readable error message to show users
- * @param {String} code
- * @returns string
- */
-function getErrorMessage(code) {
-  if (code === "404") {
-    return "Could not find that city.";
-  }
-
-  return "Something went wrong when retrieving weather data.";
-}
-
-/**
  * Fetches the current weather for given city
  * @param {String} city
  * @returns weather date or error message
  */
 export async function getWeatherForCity(city) {
-  const params = new URLSearchParams({
+  return makeWeatherAPICall({
     q: city,
-    units: "metric",
-    appid: API_KEY,
   });
-  const response = await fetch(API_BASE_URL + "?" + params.toString());
-  const result = await response.json();
-
-  if (!response.ok) {
-    return {
-      success: false,
-      error: getErrorMessage(result.cod),
-    };
-  }
-
-  return {
-    success: true,
-    data: {
-      name: result.name,
-      general: result.weather[0].main,
-      icon: result.weather[0].icon,
-      temperature: result.main.temp,
-      humidity: result.main.humidity,
-    },
-  };
 }
 
 /**
@@ -53,38 +18,16 @@ export async function getWeatherForCity(city) {
  * @returns weather date or error message
  */
 export async function getWeatherForCoords(coords) {
-  const params = new URLSearchParams({
+  return makeWeatherAPICall({
     lat: coords.latitude,
     lon: coords.longitude,
-    units: "metric",
-    appid: API_KEY,
   });
-  const response = await fetch(API_BASE_URL + "?" + params.toString());
-  const result = await response.json();
-
-  if (!response.ok) {
-    return {
-      success: false,
-      error: getErrorMessage(result.cod),
-    };
-  }
-
-  return {
-    success: true,
-    data: {
-      name: result.name,
-      general: result.weather[0].main,
-      icon: result.weather[0].icon,
-      temperature: result.main.temp,
-      humidity: result.main.humidity,
-    },
-  };
 }
 
 /**
  * Fetches forecasts for all given locations
  * @param {Array} locations
- * @returns Array
+ * @returns {Array}
  */
 export async function getWeatherForLocations(locations) {
   const data = [];
@@ -95,4 +38,51 @@ export async function getWeatherForLocations(locations) {
   }
 
   return data;
+}
+
+/**
+ * Utility function to call weather api
+ * @param {URLSearchParams} params
+ * @returns {Object} weather data
+ */
+async function makeWeatherAPICall(options) {
+  const params = new URLSearchParams({
+    units: "metric",
+    appid: API_KEY,
+    ...options,
+  });
+
+  const response = await fetch(API_BASE_URL + "?" + params.toString());
+  const result = await response.json();
+
+  if (!response.ok) {
+    return {
+      success: false,
+      error: getErrorMessage(result.cod),
+    };
+  }
+
+  return {
+    success: true,
+    data: {
+      name: result.name,
+      general: result.weather[0].main,
+      icon: result.weather[0].icon,
+      temperature: result.main.temp,
+      humidity: result.main.humidity,
+    },
+  };
+}
+
+/**
+ * Creates a human-readable error message to show users
+ * @param {String} code
+ * @returns {String} error message
+ */
+function getErrorMessage(code) {
+  if (code === "404") {
+    return "Could not find that city.";
+  }
+
+  return "Something went wrong when retrieving weather data.";
 }
