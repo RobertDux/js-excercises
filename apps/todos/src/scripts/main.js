24d4ba5f63023d917f24c1aa29136734646e5ae6
RobertDux <126151329+RobertDux@users.noreply.github.com>
2024-07-08 17:03:10 +0200

    feat: add keyboard shortcut to focus on form

--- apps/todos/src/scripts/main.js
+++ apps/todos/src/scripts/main.js
@@ -22,7 +22,36 @@
     descContainerElem.classList.toggle("d-none");
   }
 
+  /**
+   * Sets focus to first element in form
+   * @param {KeyboardEvent} e
+   */
+  function focusForm(e) {
+    e.preventDefault();
+    formElem.elements[0].focus();
+  }
+
+  /**
+   * Triggers functions based on keys pressed when not in an input
+   * @param {KeyboardEvent} e
+   */
+  function handleKeyDown(e) {
+    const { tagName, isContentEditable } = e.target;
+    if (tagName == "INPUT" || tagName == "SELECT" || tagName == "TEXTAREA" || isContentEditable) {
+      return false;
+    }
+
+    switch (e.key) {
+      case "n":
+      case "N":
+        return focusForm(e);
+      default:
+        return false;
+    }
+  }
+
   // Register event listeners
   formElem.addEventListener("submit", handleFormSubmit, false);
   descToggleElem.addEventListener("click", handleDescriptionToggle, false);
+  document.addEventListener("keydown", handleKeyDown, false);
 })();
